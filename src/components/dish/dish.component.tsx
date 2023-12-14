cc2cb07441cc99af141fdd9d3f09eb1fdc215873
maksym_bolshak <bolshakmv@gmail.com>
2023-12-14 16:49:02 +0100

    update 1.1

--- src/components/dish/dish.component.tsx
+++ src/components/dish/dish.component.tsx
@@ -1,6 +1,6 @@
-import React, { memo, useState, useMemo } from 'react';
+import React, { memo, useState, useMemo, useEffect } from 'react';
 import { IDish } from '../../types'
-import { Grid } from '@mui/material';
+import { Grid, Modal } from '@mui/material';
 import { CounterComponent } from '../counter/counter.component';
 import { useAppDispatch, useAppSelector } from '../../store/app/hooks';
 import { selectCartItems, selectFavourites, updateFavourites } from '../../store/slices/cart/cart.slice';
@@ -9,10 +9,13 @@ import { PriceComponent } from './price.component';
 import { STORAGE_KEYS } from '../../common/constants';
 import { ModeEnum } from '../../types/mode.enum';
 import { ReactComponent as FavouriteIcon } from '../../assets/svg/favorite.svg'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { selectLanguage } from '../../store/slices/menu/menu.slice';
 import styles from './style.module.css';
 import { AllergensComponent } from './allergens.component';
 import { selectTexts } from '../../store/slices/texts.slice';
+import { ExtraComponent } from './extra.component';
+import { DishDetailsComponent } from '../dishDetails';
 
 interface IProps {
   dish: IDish;
@@ -34,13 +37,19 @@ export const DishComponent: React.FC<IProps> = memo(({
   comments,
   setComments,
 }) => {
+  const [showExtras, setShowExtras] = useState(false);
   const cartItems = useAppSelector(selectCartItems);
   const favourites = useAppSelector(selectFavourites);
-  const lang = useAppSelector(selectLanguage) || 'en';
+  const lang = useAppSelector(selectLanguage) || 'EN';
   const { texts } = useAppSelector(selectTexts);
   const [choosenVolumeId, setChoosenVolumeId] = useState(volumeId || dish.dishVolumesAndPrice[0].id);
   const mode = sessionStorage.getItem(STORAGE_KEYS.MODE);
   const dispatch = useAppDispatch();
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const toggleIsOpenModal = () => {
+    setIsOpenModal(!isOpenModal);
+  }
 
   const isDishAddedToCart = useMemo(() => {
     if (mode === ModeEnum.readonly) {
@@ -48,8 +57,20 @@ export const DishComponent: React.FC<IProps> = memo(({
     } else {
       return cartItems.some((item) => item.dish.id === dish.id && item.volumeId === choosenVolumeId);
     }
-  }, [mode, cartItems, favourites, choosenVolumeId, dish.id])
+  }, [mode, cartItems, favourites, choosenVolumeId, dish.id]);
 
+  useEffect(() => { 
+    if (!isDishAddedToCart) {
+      setShowExtras(false);
+    }
+  }, [isDishAddedToCart])
+
+  
+  const handleShowExtras = () => {
+    if (!isDishAddedToCart) return;
+
+    setShowExtras(!showExtras);
+  }
 
   const changeVolumeId = (id: number) => {
     setChoosenVolumeId(id)
@@ -67,12 +88,25 @@ export const DishComponent: React.FC<IProps> = memo(({
 
   return (
     <div className={`${styles.dish} ${isDishAddedToCart ? styles.checked : ''}`}>
+      <Modal
+        open={isOpenModal}
+        onClose={toggleIsOpenModal}
+      >
+        <DishDetailsComponent
+          dish={dish}
+          onClose={toggleIsOpenModal}
+          volumeId={choosenVolumeId} 
+          setVolumeId={changeVolumeId} 
+          readonly={readonly}
+          isDishAddedToCart={isDishAddedToCart}
+        />
+      </Modal>
       <div className={styles.content}>
         <Grid container flexDirection='column' className={styles.description}>
           <Grid container flexDirection='column' justifyContent='space-between' height='100%'>
             <div className={styles.textContent}>
-              <h5 className={styles.name}>{dish.multilingualName?.[lang] || dish.name}</h5>
-              <DedscriptionComponent text={dish.multilingualDescription?.[lang] || dish.description} />
+              <h5 className={styles.name}>{dish.multilingualNameMap?.[lang] || dish.name}</h5>
+              <DedscriptionComponent text={dish.multilingualDescriptionMap?.[lang] || dish.description} />
             </div>
             <PriceComponent
               volumeId={choosenVolumeId} 
@@ -88,7 +122,7 @@ export const DishComponent: React.FC<IProps> = memo(({
         <div className={styles.side}>
           <div className={`${styles.right} ${dish?.image ? styles.rightLarge : ''}`}>
             {dish?.image && (
-              <div className={styles.box}>
+              <div className={styles.box} onClick={toggleIsOpenModal}>
                 <img src={`data:image/png;base64,${dish.image}`} alt="dish" />
               </div>
             )}
@@ -103,6 +137,26 @@ export const DishComponent: React.FC<IProps> = memo(({
           </div>
         </div>
       </div>
+        {Boolean(dish?.extras.length) && (
+          <button onClick={handleShowExtras} className={styles.showExtrasButton}>
+            Add your extra
+            <span className={`${styles.icon} ${showExtras ? styles.iconReverted : ''}`}>
+              <ExpandMoreIcon sx={{ width: 20, height: 20 }} />
+            </span>
+          </button>
+        )}
+        {showExtras && (
+          <div className={styles.extras}>
+            {dish.extras.map((extra) => (
+              <ExtraComponent 
+                key={extra.id} 
+                extra={extra} 
+                dish={dish}
+                volumeId={choosenVolumeId}
+              />
+            ))}
+          </div>
+        )}
       {isCartItem && comments && handleChangeComment && (
         <input 
           type="text" 
